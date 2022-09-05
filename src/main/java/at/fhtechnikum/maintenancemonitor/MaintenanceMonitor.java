a5c77bb585498ac8d20f23efb9e3a1135b42b787
Robert <wi21b059@technikum-wien.at>
2022-09-05 02:27:46 +0200

    implementing and tested feature getMessage

--- src/main/java/at/fhtechnikum/maintenancemonitor/MaintenanceMonitor.java
+++ src/main/java/at/fhtechnikum/maintenancemonitor/MaintenanceMonitor.java
@@ -3,4 +3,8 @@ package at.fhtechnikum.maintenancemonitor;
 public class MaintenanceMonitor {
 
     private String message = "";
+
+    public String getMessage(){
+        return this.message;
+    }
 }
